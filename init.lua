42994ed147d59477d49e0f17bab8d5e3d0221abf
cade lueker <cadelueker@cades-MacBook-Air.local>
2021-11-21 02:26:48 -0500

    bufline config

--- init.lua
+++ init.lua
@@ -105,7 +105,7 @@ require("line")
 require("bufferline").setup{
   options = {
     always_show_bufferline = false,
-    show_close_icon = false,
+    show_close_icon = true,
     show_buffer_close_icons = false,
     right_mouse_command = "buffer %d",
     offsets = {
