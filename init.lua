b6a854c65a4faa7bc5f49c0eb2e0b519fb2fa612
CadeMichael <cade.lueker@gmail.com>
2022-08-02 01:09:10 -0400

    neovide

--- init.lua
+++ init.lua
@@ -169,3 +169,8 @@ else
   -- term line num
   vim.cmd "autocmd TermOpen * setlocal nonumber norelativenumber"
 end
+
+-- Neovide
+vim.g.neovide_transparency = 0.9
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 2.0
