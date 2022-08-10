2c6b18ca0f67311c7261d9cf9138505990c2b59b
cade lueker <cadelueker@cades-MacBook-Air.local>
2022-08-09 18:48:07 -0600

    compile

--- lua/compile.lua
+++ lua/compile.lua
@@ -1,18 +1,21 @@
 local guess_command = function()
   local ft_commands = {}
+  -- create a table of ftypes
   ft_commands["go"] = "go build "
   ft_commands["javascript"] = "node "
   ft_commands["lua"] = "lua "
   ft_commands["python"] = "python "
   ft_commands["rust"] = "cargo run "
   ft_commands["zig"] = "zig build-exe "
+
+  -- get filetype
   local ft = vim.bo.filetype
-  local command
+  local command = ""
   if ft_commands[ft] ~= nil then
     command = ft_commands[ft]
-  else
-    command = ""
   end
+
+  -- instantiate guess
   local name = vim.api.nvim_buf_get_name(0)
   if command == "" then
     command = command .. name
@@ -21,21 +24,28 @@ local guess_command = function()
 end
 
 local get_dir = function()
+  -- get current dir
   local cur_dir = vim.fn.getcwd()
+  -- see if there is a lsp root dir
   local lsp_dir = vim.lsp.buf.list_workspace_folders()[1]
   if lsp_dir ~= nil then
     cur_dir = lsp_dir
   end
+  -- allow user to modify working dir
   local dir = vim.fn.input("Directory=> ", cur_dir, "file")
   return dir
 end
 
 local run_and_print = function(buff)
   print("Compile Current Project...\n")
+  -- get the working dir & command
   local dir = get_dir()
   local cmd = guess_command()
+  -- allow user to change command
   local command = vim.fn.split(vim.fn.input("", cmd, "shellcmd"))
+  -- move to output buff
   vim.api.nvim_set_current_buf(buff)
+  -- set output & run the user set command
   vim.api.nvim_buf_set_lines(buff, 0, -1, false, { "== output ==" })
   vim.fn.jobstart(command, {
     cwd = dir,
@@ -53,9 +63,11 @@ local run_and_print = function(buff)
   })
 end
 
+-- make user function
 vim.api.nvim_create_user_command("CompileCurrent",
   function()
-    local b = vim.api.nvim_create_buf(true, true)
+    -- create buffer
+    local b = vim.api.nvim_create_buf(false, true)
     vim.cmd("bel split")
     run_and_print(b)
   end, {})
