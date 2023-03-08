8d5d1a462449bf1eeed574af75ab6bb3f7a1dda8
CadeMichael <cade.lueker@gmail.com>
2023-03-08 14:54:55 -0700

    rust setup

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,7 +1,5 @@
 vim.opt_local.listchars = ({ eol = '↵', multispace = "   >" })
 
-local rt = require('rust-tools')
-
 local help = require('cFuncs.helpers')
 
 local function rustFmt()
@@ -12,38 +10,3 @@ vim.keymap.set('n', '<C-c>;', 'A;<Esc>')
 vim.keymap.set('i', '<C-c>;', '<Esc>A;')
 vim.keymap.set('n', '<C-c>r', '<cmd>RustRun<CR>')
 vim.keymap.set('n', '<C-c>f', rustFmt, {desc = "rust format file"})
-
-rt.setup({
-    tools = {
-        inlay_hints = {
-            auto = false,
-        },
-    },
-    server = {
-        on_attach = function(_, bufnr)
-            -- Hover actions
-            vim.keymap.set("n",
-                "K",
-                rt.hover_actions.hover_actions,
-                { buffer = bufnr }
-            )
-            -- Code action groups
-            vim.keymap.set("n",
-                "<Leader>a",
-                rt.code_action_group.code_action_group,
-                { buffer = bufnr }
-            )
-            -- inlay hinting
-            vim.keymap.set("n",
-                "<Leader>h",
-                rt.inlay_hints.enable,
-                { buffer = bufnr }
-            )
-            vim.keymap.set("n",
-                "<Leader>uh",
-                rt.inlay_hints.disable,
-                { buffer = bufnr }
-            )
-        end,
-    },
-})
--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -89,7 +89,46 @@ require("lazy").setup({
         end,
     },
     -- lang support
-    'simrat39/rust-tools.nvim',
+    {
+        'simrat39/rust-tools.nvim',
+        config = function()
+            local rt = require('rust-tools')
+            rt.setup({
+                tools = {
+                    inlay_hints = {
+                        auto = false,
+                    },
+                },
+                server = {
+                    on_attach = function(_, bufnr)
+                        -- Hover actions
+                        vim.keymap.set("n",
+                            "K",
+                            rt.hover_actions.hover_actions,
+                            { buffer = bufnr }
+                        )
+                        -- Code action groups
+                        vim.keymap.set("n",
+                            "<Leader>a",
+                            rt.code_action_group.code_action_group,
+                            { buffer = bufnr }
+                        )
+                        -- inlay hinting
+                        vim.keymap.set("n",
+                            "<Leader>h",
+                            rt.inlay_hints.enable,
+                            { buffer = bufnr }
+                        )
+                        vim.keymap.set("n",
+                            "<Leader>uh",
+                            rt.inlay_hints.disable,
+                            { buffer = bufnr }
+                        )
+                    end,
+                },
+            })
+        end,
+    },
     {
         'windwp/nvim-autopairs',
         config = function()
