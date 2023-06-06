58a9f026bd5864c157b6888108d1da453496fd4d
Cade Lueker <cade@localhost.localdomain>
2023-06-05 23:17:49 -0600

    cleanup

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -92,59 +92,7 @@ require("lazy").setup({
     {
         'simrat39/rust-tools.nvim',
         config = function()
-            local rt = require('rust-tools')
-            rt.setup({
-                tools = {
-                    inlay_hints = {
-                        auto = false,
-                    },
-                },
-                server = {
-                    on_attach = function(_, bufnr)
-                        -- Hover actions
-                        vim.keymap.set("n",
-                            "K",
-                            rt.hover_actions.hover_actions,
-                            { buffer = bufnr }
-                        )
-                        -- Code action groups
-                        vim.keymap.set("n",
-                            "<Leader>a",
-                            rt.code_action_group.code_action_group,
-                            { buffer = bufnr }
-                        )
-                        -- inlay hinting
-                        vim.keymap.set("n",
-                            "<Leader>h",
-                            rt.inlay_hints.enable,
-                            { buffer = bufnr }
-                        )
-                        vim.keymap.set("n",
-                            "<Leader>uh",
-                            rt.inlay_hints.disable,
-                            { buffer = bufnr }
-                        )
-                        vim.keymap.set('n',
-                            'gD',
-                            vim.lsp.buf.declaration,
-                            { buffer = bufnr })
-                        vim.keymap.set('n',
-                            'gd',
-                            vim.lsp.buf.definition,
-                            { buffer = bufnr })
-                        vim.keymap.set('n',
-                            '<space>D',
-                            vim.lsp.buf.type_definition,
-                            { buffer = bufnr })
-                        vim.keymap.set('n',
-                            '<space>rn', vim.lsp.buf.rename,
-                            { buffer = bufnr })
-                        vim.keymap.set('n',
-                            'gr', vim.lsp.buf.references,
-                            { buffer = bufnr })
-                    end,
-                },
-            })
+            require('ide.rust')
         end,
     },
     {
@@ -154,9 +102,10 @@ require("lazy").setup({
         end,
     },
     --  'lervag/vimtex',
-    'mattn/emmet-vim',
-    'preservim/nerdcommenter',
-    'jpalardy/vim-slime',
+    'mattn/emmet-vim', -- html
+    'alaviss/nim.nvim', -- nim
+    'preservim/nerdcommenter', -- comments
+    'jpalardy/vim-slime', -- repl
     -- ({["''"]}) management,
     {
         'kylechui/nvim-surround',
