95a33f7c8c9ffcd604df92dd7727bdd1e56b2945
CadeMichael <cade.lueker@gmail.com>
2023-06-23 02:21:36 -0600

    lazy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,14 +2,9 @@
 -- keymappings
 --------------
 local function map(mode, lhs, rhs, opts)
-    local keys = require("lazy.core.handler").handlers.keys
-    ---@cast keys LazyKeysHandler
-    -- do not create the keymap if a lazy keys handler exists
-    if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-        opts = opts or {}
-        opts.silent = opts.silent ~= false
-        vim.keymap.set(mode, lhs, rhs, opts)
-    end
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 -- Telescope
--- lua/ide/debug.lua
+++ lua/ide/debug.lua
@@ -43,14 +43,9 @@ dap.configurations.rust = dap.configurations.cpp
 
 -- Keymaps
 local function map(mode, lhs, rhs, opts)
-    local keys = require("lazy.core.handler").handlers.keys
-    ---@cast keys LazyKeysHandler
-    -- do not create the keymap if a lazy keys handler exists
-    if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-        opts = opts or {}
-        opts.silent = opts.silent ~= false
-        vim.keymap.set(mode, lhs, rhs, opts)
-    end
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
 end
 map('n', '<F5>', function() dap.continue() end, { desc = 'dap continue' })
 map('n', '<F10>', function() dap.step_over() end, { desc = 'dap step over' })
