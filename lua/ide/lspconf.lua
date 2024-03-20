1cb6fc6ba84a6abcc5b57fab28a53224ee78e894
CadeMichael <cade.lueker@gmail.com>
2024-03-20 23:49:51 +0000

    completion with cr

--- lua/ide/lspconf.lua
+++ lua/ide/lspconf.lua
@@ -31,7 +31,7 @@ end
 local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
 -- Make <CR> to accept selected completion item or notify coc.nvim to format
 -- <C-g>u breaks current undo, please make your own choice
-keyset("i", "<TAB>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+keyset("i", "<CR>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 
 -- Use <c-j> to trigger snippets
 keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
