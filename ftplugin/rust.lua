2db3f1093b6b8fdeca9af2461b6d77abb599f98c
Cade Michael Lueker <63083515+CadeMichael@users.noreply.github.com>
2024-07-22 11:34:43 -0600

    merge failure test

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -2,7 +2,7 @@ local ls = require("luasnip")
 local s = ls.snippet
 local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
-
+local term_handler = "merge failure"
 ls.add_snippets("rust", {
   -- let
   s("let", fmt([[
