8a0740ffca09c9dbde5f0703847e9982a4f3682c
cademichael <cade.lueker@gmail.com>
2024-09-06 12:22:36 -0600

    coq symbols

--- ftplugin/coq.lua
+++ ftplugin/coq.lua
@@ -1,5 +1,23 @@
 local keymap = vim.keymap.set
 keymap('n', '<Space>cn', '<cmd>CoqNext<CR>', { desc = "CoqNext" })
-keymap('n', '<Space>cu', '<cmd>CoqUndo<CR>', { desc = "CoqUndo" })
-keymap('n', '<Space>cs', '<cmd>CoqStart<CR>', { desc = "CoqStart" })
 keymap('n', '<Space>cq', '<cmd>CoqStop<CR>', { desc = "CoqStop" })
+keymap('n', '<Space>cs', '<cmd>CoqStart<CR>', { desc = "CoqStart" })
+keymap('n', '<Space>cu', '<cmd>CoqUndo<CR>', { desc = "CoqUndo" })
+
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+ls.add_snippets("coq", {
+  s("forall", fmt([[
+∀ {}
+]], {
+    i(0),
+  })),
+  s("exists", fmt([[
+∃ {}
+]], {
+    i(0),
+  })),
+})
