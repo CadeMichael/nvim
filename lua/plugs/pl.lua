de8a5a71d6caad53152b55cedbefc99ebd504d92
cademichael <cade.lueker@gmail.com>
2024-09-12 11:01:23 -0600

    coq and lean4

--- lua/plugs/pl.lua
+++ lua/plugs/pl.lua
@@ -10,4 +10,20 @@ return {
     end
   },
   'whonore/Coqtail',
+  {
+    'Julian/lean.nvim',
+    event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'nvim-lua/plenary.nvim',
+      -- you also will likely want nvim-cmp or some completion engine
+    },
+
+    -- see details below for full configuration options
+    opts = {
+      lsp = {},
+      mappings = true,
+    }
+  }
 }
