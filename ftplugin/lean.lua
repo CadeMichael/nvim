fd341b2dffc3dc9ebd322c5edd914f8ed6b03286
CadeMichael <cade.lueker@gmail.com>
2024-11-27 01:03:45 -0500

    lean comments

--- ftplugin/lean.lua
+++ ftplugin/lean.lua
@@ -2,7 +2,22 @@ local keymap = vim.keymap.set
 local bufnr = vim.api.nvim_get_current_buf()
 local opts = { noremap = true, silent = true, buffer = bufnr, }
 
+-- Local bindings
 keymap('n', '<Space>lr', '<cmd>LeanRestartFile<CR>', opts)
 keymap('n', '<Space>lv', '<cmd>LeanInfoviewToggle<CR>', opts)
 keymap('n', '<Space>lt', '<cmd>LeanTermGoal<CR>', opts)
 keymap('n', '<Tab>', '<cmd>LeanLineDiagnostics<CR>', opts)
+
+-- Snippets
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+ls.add_snippets("lean", {
+  s("--", fmt([[
+/-! {} -/
+  ]], {
+    i(0),
+  })),
+})
