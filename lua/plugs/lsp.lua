fb211d69bd99367f6dd53307f483b234a4c54a14
CadeMichael <cade.lueker@gmail.com>
2025-01-10 02:39:30 -0700

    lsp keymaps

--- lua/plugs/lsp.lua
+++ lua/plugs/lsp.lua
@@ -3,7 +3,7 @@ return {
     'folke/trouble.nvim',
     config = function()
       require('trouble').setup({})
-      vim.keymap.set('n', '<Space>td', "<cmd>Trouble diagnostics toggle<CR>", { desc = "trouble diagnostic toggle" })
+      vim.keymap.set('n', '<Space>td', '<cmd>Trouble diagnostics toggle<CR>', { desc = 'trouble diagnostic toggle' })
     end
   },
   {
@@ -25,8 +25,9 @@ return {
         end,
         jump = function(direction) require('luasnip').jump(direction) end,
       },
-      keymap = { preset = 'default' },
-
+      keymap = {
+        preset = 'default',
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
@@ -35,20 +36,79 @@ return {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
     },
-    opts_extend = { "sources.default" }
+    opts_extend = { 'sources.default' },
   },
   'folke/neodev.nvim', -- lua
   {
     'neovim/nvim-lspconfig',
     dependencies = { 'saghen/blink.cmp' },
     config = function()
+      local tsb = require('telescope.builtin')
+      local on_attach = function(_, bufnr)
+        -- Mappings.
+        local function get_opts(desc)
+          return { desc = desc, buffer = bufnr, noremap = true, silent = true }
+        end
+        -- formatting
+        vim.keymap.set(
+          'n',
+          '<space>f',
+          function()
+            vim.lsp.buf.format()
+          end,
+          get_opts('format buffer')
+        )
+        -- renaming
+        vim.keymap.set(
+          'n',
+          '<space>rn',
+          function()
+            vim.lsp.buf.rename()
+          end,
+          get_opts('rename')
+        )
+        -- gotos
+        vim.keymap.set(
+          'n',
+          'gd',
+          function()
+            vim.lsp.buf.definition()
+          end,
+          get_opts('goto definition')
+        )
+        vim.keymap.set('n',
+          'gr',
+          function()
+            tsb.lsp_references()
+          end,
+          get_opts('telescope get references')
+        )
+        -- diagnostics
+        vim.keymap.set(
+          'n',
+          '<space>E',
+          tsb.diagnostics,
+          get_opts('telescope diagnostics')
+        )
+        vim.keymap.set(
+          'n',
+          '<space>ee',
+          vim.diagnostic.open_float,
+          get_opts('diagnostic open float')
+        )
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, get_opts('prev diagnostic'))
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, get_opts('next diagnostic'))
+      end
+
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       local lsp = require('lspconfig')
+
       --> lua
       -- neovim lsp
       require("neodev").setup({})
       lsp.lua_ls.setup {
         capabilities = capabilities,
+        on_attach = on_attach
       }
       --> C
       lsp.clangd.setup({
@@ -75,7 +135,7 @@ return {
         lsp = {
           capabilities = capabilities,
           handlers = {
-            ["textDocument/publishDiagnostics"] = vim.lsp.with(
+            ['textDocument/publishDiagnostics'] = vim.lsp.with(
               vim.lsp.diagnostic.on_publish_diagnostics, {
                 -- Disable virtual_text
                 virtual_text = false
@@ -94,7 +154,7 @@ return {
         settings = {
           nixd = {
             formatting = {
-              command = { "nixfmt" },
+              command = { 'nixfmt' },
             },
           },
         },
