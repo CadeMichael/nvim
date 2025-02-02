a617d8733222027f1fab00e9487855e9bdcdcdd7
CadeMichael <cade.lueker@gmail.com>
2025-02-01 18:32:44 -0700

    conjure

--- lua/plugs/editor.lua
+++ lua/plugs/editor.lua
@@ -172,6 +172,17 @@ return {
     end
   },
   {
+    "Olical/conjure",
+    ft = { "python" },
+    lazy = true,
+    init = function()
+      -- Set configuration options here
+      -- Uncomment this to get verbose logging to help diagnose internal Conjure issues
+      -- This is VERY helpful when reporting an issue with the project
+      -- vim.g["conjure#debug"] = true
+    end
+  },
+  {
     "jubnzv/mdeval.nvim",
     config = function()
       require 'mdeval'.setup({
