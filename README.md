dba70a1587111f3b5cf547d046851bda2f3b75ba
CadeMichael <cade.lueker@gmail.com>
2025-02-05 15:13:33 -0700

    markdown notes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Neovim Conf
+
+- main plugins
+    + lazy nvim
+    + nvim lspconfig
+    + blink cmp
+    + snacks nvim
+    + telescope
--- ftplugin/telekasten.lua
+++ ftplugin/telekasten.lua
@@ -1,12 +1,6 @@
--- local keymap = vim.keymap.set
--- local bufnr = vim.api.nvim_get_current_buf()
+local keymap = vim.keymap.set
+local bufnr = vim.api.nvim_get_current_buf()
+local opts = { noremap = true, silent = true, buffer = bufnr, }
 
--- local glow = function()
-  -- local bufid = vim.api.nvim_get_current_buf()
-  -- local buf = vim.api.nvim_buf_get_name(bufid)
-  -- local cmd = "FloatermNew glow -p " .. buf
-  -- print(cmd)
-  -- vim.cmd(cmd)
--- end
-
--- keymap('n', '<Space>G', glow, { buffer = bufnr, desc = 'preview note file' })
+-- Keybindings
+keymap({'n', 'i'}, '<C-c><C-c>', '<cmd>MdEval<CR>', opts)
