094e166da775e1010f9b0c720c89b0785a26d2ac
CadeMichael <cade.lueker@gmail.com>
2025-02-25 14:09:47 -0700

    markdown snippets

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -3,4 +3,23 @@ local bufnr = vim.api.nvim_get_current_buf()
 local opts = { noremap = true, silent = true, buffer = bufnr, }
 
 -- Keybindings
-keymap({'n', 'i'}, '<C-c><C-c>', '<cmd>MdEval<CR>', opts)
+keymap({ 'n', 'i' }, '<C-c><C-c>', '<cmd>MdEval<CR>', opts)
+
+-- Snippets
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+ls.add_snippets("markdown", {
+  s("<s", fmt([[
+  ```<>
+  <>
+  ```
+  ]], {
+    i(1, "lang"),
+    i(0),
+  }, {
+    delimiters = "<>"
+  })),
+})
