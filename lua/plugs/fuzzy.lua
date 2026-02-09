3287b38871654e5e13919c18ac84a4ce9df8a929
CadeMichael <cade.lueker@gmail.com>
2026-02-09 11:00:54 -0700

    fzf over telescope

--- lua/plugs/fuzzy.lua
+++ lua/plugs/fuzzy.lua
@@ -1,28 +1,18 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        config = function()
-          require('telescope').setup({})
-          require('telescope').load_extension('fzf')
-        end,
-      },
-    },
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<space>f', builtin.find_files, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<space>/', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<space>g/', builtin.git_status, { desc = 'Telescope git status' })
-      vim.keymap.set('n', '<space>gb', builtin.git_branches, { desc = 'Telescope git branches' })
-      vim.keymap.set('n', '<space>,', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<space>h', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<space>m', builtin.keymaps, { desc = 'Telescope keymaps' })
-      vim.keymap.set('n', '<space>E', builtin.diagnostics, { desc = 'Telescope diagnostics' })
-      vim.keymap.set('n', 'grr', builtin.lsp_references, { desc = 'Telescope LSP references' })
+      local fzf = require('fzf-lua')
+      vim.keymap.set('n', '<space>f', fzf.files, { desc = 'find files' })
+      vim.keymap.set('n', '<space>/', fzf.live_grep, { desc = 'live grep' })
+      vim.keymap.set('n', '<space>gf', fzf.git_files, { desc = 'git files' })
+      vim.keymap.set('n', '<space>gb', fzf.git_branches, { desc = 'git branches' })
+      vim.keymap.set('n', '<space>,', fzf.buffers, { desc = 'find buffers' })
+      vim.keymap.set('n', '<space>h', fzf.helptags, { desc = 'helptags' })
+      vim.keymap.set('n', '<space>m', fzf.keymaps, { desc = 'keymaps' })
+      vim.keymap.set('n', '<space>E', fzf.diagnostics_document, { desc = 'diagnostics document' })
+      vim.keymap.set('n', 'grr', fzf.lsp_references, { desc = 'lsp references' })
     end
   },
 }
