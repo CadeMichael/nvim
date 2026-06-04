46fb98dec735bcb17a6b2d09fde4f1316e8cfe56
cademichael <cade.lueker@gmail.com>
2026-06-04 14:32:55 -0600

    markdown todos

--- init.lua
+++ init.lua
@@ -32,7 +32,9 @@ vim.pack.add({
 	},
 	-- completion
 	'https://github.com/saghen/blink.lib',
-	'https://github.com/saghen/blink.cmp'
+	'https://github.com/saghen/blink.cmp',
+	-- notes
+	'https://github.com/bngarren/checkmate.nvim'
 })
 
 -- floaterm maps
@@ -129,6 +131,90 @@ cmp.setup({
     completion = { documentation = { auto_show = false } },
     sources = { default = { 'lsp', 'path', 'buffer' } },
     fuzzy = { implementation = "rust" }
+})
+
+-- notes
+require('checkmate').setup({
+	todo_states = {
+		in_progress = {
+			marker = "◐",
+			markdown = ".",     -- Saved as `- [.]`
+			type = "incomplete", -- Counts as "not done"
+			order = 50,
+		},
+	},
+	keys = {
+    ["<space>tt"] = {
+      rhs = "<cmd>Checkmate toggle<CR>",
+      desc = "Toggle todo item",
+      modes = { "n", "v" },
+    },
+    ["<space>tc"] = {
+      rhs = "<cmd>Checkmate check<CR>",
+      desc = "Set todo item as checked (done)",
+      modes = { "n", "v" },
+    },
+    ["<space>tu"] = {
+      rhs = "<cmd>Checkmate uncheck<CR>",
+      desc = "Set todo item as unchecked (not done)",
+      modes = { "n", "v" },
+    },
+    ["<space>t="] = {
+      rhs = "<cmd>Checkmate cycle_next<CR>",
+      desc = "Cycle todo item(s) to the next state",
+      modes = { "n", "v" },
+    },
+    ["<space>t-"] = {
+      rhs = "<cmd>Checkmate cycle_previous<CR>",
+      desc = "Cycle todo item(s) to the previous state",
+      modes = { "n", "v" },
+    },
+    ["<space>tn"] = {
+      rhs = "<cmd>Checkmate create<CR>",
+      desc = "Create todo item",
+      modes = { "n", "v" },
+    },
+    ["<space>tr"] = {
+      rhs = "<cmd>Checkmate remove<CR>",
+      desc = "Remove todo marker (convert to text)",
+      modes = { "n", "v" },
+    },
+    ["<space>tR"] = {
+      rhs = "<cmd>Checkmate remove_all_metadata<CR>",
+      desc = "Remove all metadata from a todo item",
+      modes = { "n", "v" },
+    },
+    ["<space>ta"] = {
+      rhs = "<cmd>Checkmate archive<CR>",
+      desc = "Archive checked/completed todo items (move to bottom section)",
+      modes = { "n" },
+    },
+    ["<space>tF"] = {
+      rhs = "<cmd>Checkmate select_todo<CR>",
+      desc = "Open a picker to select a todo from the current buffer",
+      modes = { "n" },
+    },
+    ["<space>tv"] = {
+      rhs = "<cmd>Checkmate metadata select_value<CR>",
+      desc = "Update the value of a metadata tag under the cursor",
+      modes = { "n" },
+    },
+    ["<space>t]"] = {
+      rhs = "<cmd>Checkmate metadata jump_next<CR>",
+      desc = "Move cursor to next metadata tag",
+      modes = { "n" },
+    },
+    ["<space>t["] = {
+      rhs = "<cmd>Checkmate metadata jump_previous<CR>",
+      desc = "Move cursor to previous metadata tag",
+      modes = { "n" },
+    },
+    ["<space>tp"] = {
+      rhs = '<cmd>lua require("checkmate").toggle("in_progress")<CR>',
+      desc = "Set todo item to in progress.",
+      modes = { "n" },
+    },
+  },
 })
 
 -- autocommands
